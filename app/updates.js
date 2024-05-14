f46c38c059bf17abec019beda6403a579a634294
gifteddevs <168932918+gifteddevs@users.noreply.github.com>
2024-05-14 16:52:21 +0300

    Update updates.js

--- app/updates.js
+++ app/updates.js
@@ -1,57 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { gifted } = require("../gifted/gifted");
-gifted({ nomCom: "wagroup", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join the OFFICIAL Gifted-Md WhatsApp Group\n\n';
-    let d = 'https://chat.whatsapp.com/ExdDIwx7j36Ci7VP0TbCYw';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
 
-gifted({ nomCom: "channel", reaction: "🤍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join the OFFICIAL Gifted-Md WhatsApp Channel.\n\n';
-    let d = 'https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
 
-gifted({ nomCom: "channel2", reaction: "🤍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join the OFFICIAL Gifted-Tech WhatsApp Channel.\n\n';
-    let d = 'https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
 
-gifted({ nomCom: "community", reaction: "🤍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join the OFFICIAL Gifted-Md WhatsApp Community.\n\n';
-    let d = 'https://chat.whatsapp.com/J6Gc8hVrTxS0dq4e02lS7H';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
+'use strict';
 
-gifted({ nomCom: "discussion", reaction: "🤍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = 'Hello 👋\n\nClick The link below to Join the OFFICIAL Gifted-Md WhatsApp Discussion Group.\n\n';
-    let d = 'https://chat.whatsapp.com/Czb6vkT4oaKLg80VN74QSA';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
+
+
+
+
+
+
+
+function _0x2452(_0x4753be,_0x2009b4){const _0x30658a=_0x3065();return _0x2452=function(_0x24525c,_0x3097d5){_0x24525c=_0x24525c-0x1a3;let _0xaf27b3=_0x30658a[_0x24525c];return _0xaf27b3;},_0x2452(_0x4753be,_0x2009b4);}const _0x20500a=_0x2452;(function(_0x47c495,_0x18e4ea){const _0x35f412=_0x2452,_0x2bb486=_0x47c495();while(!![]){try{const _0x3aa16b=parseInt(_0x35f412(0x1ba))/0x1*(-parseInt(_0x35f412(0x1ab))/0x2)+-parseInt(_0x35f412(0x1a4))/0x3*(-parseInt(_0x35f412(0x1ac))/0x4)+parseInt(_0x35f412(0x1b9))/0x5*(parseInt(_0x35f412(0x1a9))/0x6)+-parseInt(_0x35f412(0x1c0))/0x7*(-parseInt(_0x35f412(0x1be))/0x8)+parseInt(_0x35f412(0x1b0))/0x9*(parseInt(_0x35f412(0x1a3))/0xa)+-parseInt(_0x35f412(0x1a5))/0xb*(parseInt(_0x35f412(0x1c2))/0xc)+-parseInt(_0x35f412(0x1b1))/0xd;if(_0x3aa16b===_0x18e4ea)break;else _0x2bb486['push'](_0x2bb486['shift']());}catch(_0x411ffb){_0x2bb486['push'](_0x2bb486['shift']());}}}(_0x3065,0x69e8a));Object[_0x20500a(0x1bc)](exports,'__esModule',{'value':!![]});function _0x3065(){const _0x381f19=['1574eUdXRw','184CBDDlM','sendMessage','channel2','Hello\x20👋\x0a\x0aClick\x20The\x20link\x20below\x20to\x20Join\x20the\x20OFFICIAL\x20Gifted-Tech\x20WhatsApp\x20Channel.\x0a\x0a','9pRRfmF','8632949GIKebY','log','Hello\x20👋\x0a\x0aClick\x20The\x20link\x20below\x20to\x20Join\x20the\x20OFFICIAL\x20Gifted-Md\x20WhatsApp\x20Community.\x0a\x0a','https://chat.whatsapp.com/J6Gc8hVrTxS0dq4e02lS7H','channel','Hello\x20👋\x0a\x0aClick\x20The\x20link\x20below\x20to\x20Join\x20the\x20OFFICIAL\x20Gifted-Md\x20WhatsApp\x20Discussion\x20Group.\x0a\x0a','../gifted/gifted','https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l','545OqmBqx','615YjEQch','community','defineProperty','Hello\x20👋\x0a\x0aClick\x20The\x20link\x20below\x20to\x20Join\x20the\x20OFFICIAL\x20Gifted-Md\x20WhatsApp\x20Channel.\x0a\x0a','90352huOwXr','https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg','434NBGutb','mon\x20test','276xoHdmO','Commande\x20saisie\x20!!!s','4453930uNThuC','6813KlfUqi','221243bOacFP','https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o','https://chat.whatsapp.com/Czb6vkT4oaKLg80VN74QSA','discussion','43728YBrJox','wagroup'];_0x3065=function(){return _0x381f19;};return _0x3065();}const {gifted}=require(_0x20500a(0x1b7));gifted({'nomCom':_0x20500a(0x1aa),'reaction':'😌','nomFichier':__filename},async(_0x19910e,_0x316cb2,_0x54a8f6)=>{const _0x41168f=_0x20500a;console[_0x41168f(0x1b2)](_0x41168f(0x1c3));let _0x46157a='Hello\x20👋\x0a\x0aClick\x20The\x20link\x20below\x20to\x20Join\x20the\x20OFFICIAL\x20Gifted-Md\x20WhatsApp\x20Group\x0a\x0a',_0x5b6432='https://chat.whatsapp.com/ExdDIwx7j36Ci7VP0TbCYw',_0xbd5baa=_0x46157a+_0x5b6432;var _0x443add=_0x41168f(0x1bf);await _0x316cb2[_0x41168f(0x1ad)](_0x19910e,{'image':{'url':_0x443add},'caption':_0xbd5baa});}),console[_0x20500a(0x1b2)](_0x20500a(0x1c1)),gifted({'nomCom':_0x20500a(0x1b5),'reaction':'🤍','nomFichier':__filename},async(_0x19b0e3,_0x15a64b,_0x4e3eca)=>{const _0x337c58=_0x20500a;console[_0x337c58(0x1b2)](_0x337c58(0x1c3));let _0x2addd8=_0x337c58(0x1bd),_0x6e046d=_0x337c58(0x1b8),_0x480523=_0x2addd8+_0x6e046d;var _0x3a52ac=_0x337c58(0x1bf);await _0x15a64b['sendMessage'](_0x19b0e3,{'image':{'url':_0x3a52ac},'caption':_0x480523});}),console[_0x20500a(0x1b2)](_0x20500a(0x1c1)),gifted({'nomCom':_0x20500a(0x1ae),'reaction':'🤍','nomFichier':__filename},async(_0x2fd30c,_0x2770eb,_0x135dc9)=>{const _0x53bddd=_0x20500a;console['log'](_0x53bddd(0x1c3));let _0x50eda6=_0x53bddd(0x1af),_0x3b512c=_0x53bddd(0x1a6),_0x34b5de=_0x50eda6+_0x3b512c;var _0x2e5229=_0x53bddd(0x1bf);await _0x2770eb[_0x53bddd(0x1ad)](_0x2fd30c,{'image':{'url':_0x2e5229},'caption':_0x34b5de});}),console[_0x20500a(0x1b2)](_0x20500a(0x1c1)),gifted({'nomCom':_0x20500a(0x1bb),'reaction':'🤍','nomFichier':__filename},async(_0x2a31b9,_0x3bd573,_0x26229d)=>{const _0x11b6ae=_0x20500a;console[_0x11b6ae(0x1b2)](_0x11b6ae(0x1c3));let _0x5cd4d3=_0x11b6ae(0x1b3),_0x554ef5=_0x11b6ae(0x1b4),_0x823e0d=_0x5cd4d3+_0x554ef5;var _0x5df123=_0x11b6ae(0x1bf);await _0x3bd573[_0x11b6ae(0x1ad)](_0x2a31b9,{'image':{'url':_0x5df123},'caption':_0x823e0d});}),console[_0x20500a(0x1b2)]('mon\x20test'),gifted({'nomCom':_0x20500a(0x1a8),'reaction':'🤍','nomFichier':__filename},async(_0x2a00f8,_0x4e9e86,_0x871b7b)=>{const _0x38bf4=_0x20500a;console[_0x38bf4(0x1b2)](_0x38bf4(0x1c3));let _0x5ba7ef=_0x38bf4(0x1b6),_0x5901b5=_0x38bf4(0x1a7),_0x562919=_0x5ba7ef+_0x5901b5;var _0x13ff28=_0x38bf4(0x1bf);await _0x4e9e86['sendMessage'](_0x2a00f8,{'image':{'url':_0x13ff28},'caption':_0x562919});}),console['log']('mon\x20test');
+
+
