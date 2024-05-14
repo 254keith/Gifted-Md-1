ad11e2d93fdd8056f7b316e73d17e68be6933bea
gifteddevs <168932918+gifteddevs@users.noreply.github.com>
2024-05-14 07:03:17 +0300

    Update and rename SC.js to gitgifted.js

---
+++ app/gitgifted.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { gifted } = require("../gifted/gifted");
+gifted({ nomCom: "gifted",reaction: "😌"}, async (dest, zk, commandeOptions) => {
+
+
+const githubRepo = 'https://api.github.com/repos/mouricedevs/Gifted-Md';
+
+const img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
+
+    const response = await fetch(githubRepo); 
+        const data = await response.json(); 
+ 
+        if (data) {
+            const repoInfo = {
+                stars: data.stargazers_count,
+                forks: data.forks_count,
+                
+                
+               
+            };
+
+
+const gitdata = `
+*A Total of ${repoInfo.forks} People are have Deployed and are Currently using Gifted-Md.*
+
+*${repoInfo.stars} People have starred it Showing a sign of Loving it.*
+
+*ɢɪғᴛᴇᴅ-ᴍᴅ ᴠᴇʀsɪᴏɴ 3.0.0*`;
+
+
+
+
+await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
+
+} else {
+console.log("Could not fetch data")
+
+}
+
+
+});
