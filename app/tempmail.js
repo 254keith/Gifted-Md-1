b79fd027a6c782f69608a50500f99f1b172329d2
gifteddevs <168932918+gifteddevs@users.noreply.github.com>
2024-05-14 16:50:49 +0300

    Update tempmail.js

--- app/tempmail.js
+++ app/tempmail.js
@@ -1,12 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { gifted } = require("../gifted/gifted");
-gifted({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = '*Gifted-Md* Temporary Emails: \n \n ' + "Click the links below to create unlimited TEMPORARY Emails. *ɢɪғᴛᴇᴅ ᴍᴅ ᴠᴇʀsɪᴏɴ 3.0.0*\n\n";
-    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
-    let varmess = z + d;
-    var img = 'https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg';
-    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
-    //console.log("montest")
-});
+'use strict';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x29fc62=_0x558e;(function(_0xe8ee5d,_0x317bd9){const _0x2fc86d=_0x558e,_0x127b9c=_0xe8ee5d();while(!![]){try{const _0x4d08c3=parseInt(_0x2fc86d(0x181))/0x1*(-parseInt(_0x2fc86d(0x185))/0x2)+-parseInt(_0x2fc86d(0x175))/0x3+parseInt(_0x2fc86d(0x180))/0x4+parseInt(_0x2fc86d(0x183))/0x5*(-parseInt(_0x2fc86d(0x17c))/0x6)+-parseInt(_0x2fc86d(0x184))/0x7*(-parseInt(_0x2fc86d(0x178))/0x8)+-parseInt(_0x2fc86d(0x17e))/0x9*(parseInt(_0x2fc86d(0x182))/0xa)+-parseInt(_0x2fc86d(0x177))/0xb*(parseInt(_0x2fc86d(0x17f))/0xc);if(_0x4d08c3===_0x317bd9)break;else _0x127b9c['push'](_0x127b9c['shift']());}catch(_0x536260){_0x127b9c['push'](_0x127b9c['shift']());}}}(_0x813a,0x985d3));function _0x558e(_0x12ba67,_0x51a3d2){const _0x813a25=_0x813a();return _0x558e=function(_0x558e08,_0x20bd70){_0x558e08=_0x558e08-0x175;let _0x5754f9=_0x813a25[_0x558e08];return _0x5754f9;},_0x558e(_0x12ba67,_0x51a3d2);}Object['defineProperty'](exports,_0x29fc62(0x17b),{'value':!![]});const {gifted}=require('../gifted/gifted');gifted({'nomCom':_0x29fc62(0x176),'reaction':'😌','nomFichier':__filename},async(_0x51fb81,_0x401988,_0x174baf)=>{const _0x128237=_0x29fc62;console['log']('Commande\x20saisie\x20!!!s');let _0x52b34d='*Gifted-Md*\x20Temporary\x20Emails:\x20\x0a\x20\x0a\x20'+'Click\x20the\x20links\x20below\x20to\x20create\x20unlimited\x20TEMPORARY\x20Emails.\x20*ɢɪғᴛᴇᴅ\x20ᴍᴅ\x20ᴠᴇʀsɪᴏɴ\x203.0.0*\x0a\x0a',_0x5b153a=_0x128237(0x17d),_0x296a93=_0x52b34d+_0x5b153a;var _0xe3f691=_0x128237(0x17a);await _0x401988[_0x128237(0x179)](_0x51fb81,{'image':{'url':_0xe3f691},'caption':_0x296a93});});function _0x813a(){const _0x4f2a54=['8yKxyig','sendMessage','https://telegra.ph/file/a202f454c9532c3f5b7f8.jpg','__esModule','121962POXgDE','\x201️⃣\x20https://tempumail.com\x0a\x0a\x202️⃣\x20https://etempmail.com\x0a\x0a\x203️⃣\x20https://ghostmail.one\x0a\x0a\x204️⃣\x20https://tempmailid.com','154107phxZsW','133548mttUWr','3969744LfapIy','7909Elglmq','90TOiNTp','130YNepCU','7400694KLtEqj','22BtqVAY','565707pimKdc','tempmail','462sLrQpR'];_0x813a=function(){return _0x4f2a54;};return _0x813a();}
